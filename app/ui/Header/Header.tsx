2e5a3d2bcd8eafab289b0b4cf73213df39d8b31d
Sakib Hassan <sakibhassan.webdev@gmail.com>
2024-11-09 15:48:39 +0600

    add: profile dropdown menu

--- app/ui/Header/Header.tsx
+++ app/ui/Header/Header.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Link from "next/link";
 import { MdOutlineSearch } from "react-icons/md";
 import { FaRegUser } from "react-icons/fa";
+import LoginDropDown from "./LoginDropDown";
+import { useState } from "react";
 
 const Header = () => {
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+
   return (
     <>
       <header className="flex gap-4 items-center justify-between h-[88px] px-5 absolute top-0 left-0 right-0 text-white">
@@ -17,10 +23,20 @@ const Header = () => {
           />
           <MdOutlineSearch className="cursor-pointer absolute right-0 top-1/2 -translate-y-1/2 text-2xl text-white mx-4" />
         </div>
-        <div className="icons">
-          <div className="user-icon p-3 rounded-md hover:bg-[#ffffff0d] group cursor-pointer ease-out duration-200 text-white">
-            <FaRegUser className="text-2xl text-gray-400 group-hover:text-gray-100" />
+        <div className="dropdown">
+          <div
+            onClick={() => setIsDropDownOpen(!isDropDownOpen)}
+            className={`${
+              isDropDownOpen ? "bg-[#ffffff0d]" : ""
+            } user-icon p-3 rounded-md hover:bg-[#ffffff0d] group cursor-pointer ease-out duration-200 text-white`}
+          >
+            <FaRegUser
+              className={`${
+                isDropDownOpen ? "text-gray-100" : ""
+              } text-2xl text-gray-400 group-hover:text-gray-100`}
+            />
           </div>
+          <LoginDropDown isDropDownOpen={isDropDownOpen} />
         </div>
       </header>
     </>
